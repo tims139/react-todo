2620843bef03c186b1060de90c88b431eff06854
Timur Zinnatullin <timo@Tim-Mac.lan>
2024-03-04 15:52:44 -0800

    Lesson 1.4 completed

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
+
 const AddTodoForm = ({onAddTodo}) => {
+
+	const [todoTitle, setTodoTitle] = useState('');
+
+	const handleTitleChange = (e) => {
+		const newTodoTitle = e.target.value;
+		setTodoTitle(newTodoTitle);
+	}
 	
-	const handleAddTodo = (event) => {
-		event.preventDefault();
-		const todoTitle = event.target.elements.title.value;
-		onAddTodo(todoTitle);
-		event.target.reset();
+	const handleAddTodo = (e) => {
+		e.preventDefault();
+		const newTodo = {
+			id: Date.now(),
+			title: todoTitle
+		};
+		onAddTodo(newTodo);
+		setTodoTitle('');
 	}
 	
 	return(
 		<form onSubmit={handleAddTodo}>
 			<label htmlFor="todoTitle">Title</label>
-			<input id="todoTitle" type="text" name="title" />
+			<input id="todoTitle" type="text" name="title" value={todoTitle} onChange={handleTitleChange} />
 			<button type="submit">Add</button>
 		</form>
 	)
--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
 const App = () => {
-	const [newTodo, setNewTodo] = useState('');
+	
+	const [todoList, setTodoList] = useState([]);
+
+	const addTodo = (newTodo) => {
+		setTodoList([...todoList, newTodo]);
+	}
 
   return (
     <div>
       <h1>Todo List</h1>
-			<AddTodoForm onAddTodo={setNewTodo} />
-			<p>{newTodo}</p>
-      <TodoList />
+			<AddTodoForm onAddTodo={addTodo} />
+      <TodoList todoList={todoList} />
     </div>
   );
 }
