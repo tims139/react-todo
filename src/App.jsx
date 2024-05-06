8d7cdcc8f0579268c5d5262cf7338a55f04156a8
Timur Zinnatullin <timo@Tim-Mac.lan>
2024-05-06 13:24:17 -0700

    Lesson 1.9 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
@@ -73,17 +74,19 @@ const App = () => {
     fetchData();
   }, []);
 
-  useEffect(() => {
-    if (!isLoading)
-      localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-  }, [todoList]);
-
   return (
-    <>
-      <h1>Todo List</h1>
-      <AddTodoForm onAddTodo={addTodo} />
-      {isLoading ? (<p>Loading ...</p>) : (<TodoList todoList={todoList} onRemoveTodo={removeTodo} />)}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <h1>Todo List</h1>
+            <AddTodoForm onAddTodo={addTodo} />
+            {isLoading ? (<p>Loading...</p>) : (<TodoList todoList={todoList} onRemoveTodo={removeTodo} />)}
+          </>
+        } />
+        <Route path="/new" element={<h1>New Todo List</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
